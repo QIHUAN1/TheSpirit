f026617b130bb9e97a273f910c3643f37ce4179b
QIHUAN1 <qjiang1@inside.artcenter.edu>
2023-10-03 08:14:01 -0700

    2

---
+++ Assets/Script/Bullte/bulletControl2.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletControl2 : MonoBehaviour
+{
+    private bool hasHit = false;
+
+    [SerializeField] private bool canRelease = false;
+
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Release();
+
+    }
+
+    void Release()
+    {
+        if (canRelease == true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                EnemyAI enemyAI = gameObject.GetComponentInParent<EnemyAI>();
+                if (enemyAI != null)
+                {
+                    enemyAI.playerSpell = true;
+                    Destroy(gameObject);
+                }
+
+            }
+        }
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!hasHit && collision.gameObject.CompareTag("Monster"))
+        {
+            // Attach the bullet to the enemy
+            Transform enemyTransform = collision.gameObject.transform;
+            transform.SetParent(enemyTransform);
+
+            // Disable any movement or physics on the bullet
+            Rigidbody bulletRigidbody = GetComponent<Rigidbody>();
+            if (bulletRigidbody != null)
+            {
+                bulletRigidbody.isKinematic = true;
+                bulletRigidbody.velocity = Vector3.zero;
+            }
+
+
+            hasHit = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cylinder"))
+        {
+
+            canRelease = true;
+            Debug.Log("Player collided with the cube!");
+
+
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Cylinder"))
+        {
+
+            canRelease = false;
+            Debug.Log("Player go away!");
+        }
+    }
+}
--- Assets/Script/EnemyAI/EnemyAI.cs
+++ Assets/Script/EnemyAI/EnemyAI.cs
@@ -2,23 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class EnemyAI : MonoBehaviour
 {
     public float HP;
     [SerializeField] TextMeshProUGUI Healthpoint;
 
+    //control mode
+    public Transform player;
+    public float followspeed = 5f;
+    private Vector3 initialOffset;
+    public bool starttest;
+
+
+    public bool playerSpell = false;
+    public GameObject timer;
+    public Slider slider;
+    float countdown = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
         HP = 3;
+        timer.SetActive(false);
+        starttest = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Healthpoint.text = "HP: " + HP.ToString("0");
+
+        if(playerSpell == true)
+        {
+            ControlMode();
+        }
+
+        if (starttest == true)
+        {
+            initialOffset = transform.position - player.position;
+        }
+        else
+        {
+            
+        }
     }
+
+    void ControlMode()
+
+    {
+        starttest = true;
+        starttest = false;
+
+        Vector3 targetPosition = player.position + initialOffset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followspeed * Time.deltaTime);
+
+        timer.SetActive(true);
+        countdown -= 1 * Time.deltaTime;
+        slider.value = countdown;
+        if(countdown <= 0)
+        {
+            countdown = 0;
+            playerSpell = false;
+            countdown = 20;
+        }
+    }
+
 
     public void TakeDamage ()
     {
