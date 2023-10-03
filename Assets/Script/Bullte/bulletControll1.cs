20df3367f31ec13a964632ba57bb209a68220817
QIHUAN1 <qjiang1@inside.artcenter.edu>
2023-10-03 05:44:09 -0700

    1

--- Assets/Script/Bullte/bulletControll1.cs
+++ Assets/Script/Bullte/bulletControll1.cs
@@ -6,7 +6,6 @@ public class bulletControll1 : MonoBehaviour
 {
 
     private bool hasHit = false;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -39,6 +38,14 @@ public class bulletControll1 : MonoBehaviour
 
             // Mark as hit to prevent multiple attachments
             hasHit = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CircleController circleController = other.GetComponent<CircleController>();
+            circleController.circleStart = true;
         }
     }
 }
