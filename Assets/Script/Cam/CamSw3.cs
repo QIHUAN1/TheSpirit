5f8a3ca726d2a253e2f250eb549eef922e8af2da
QIHUAN1 <71250585+QIHUAN1@users.noreply.github.com>
2023-10-09 18:46:31 -0700

    L

--- Assets/Script/Cam/CamSw3.cs
+++ Assets/Script/Cam/CamSw3.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class CamSw3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        RoomManager rm = gameObject.GetComponentInParent<RoomManager>();
+        rm.Cube3();
     }
 }
