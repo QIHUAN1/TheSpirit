a7f7ebad7a9fddd249a55a82ed98dee7d5fb6c92
QIHUAN1 <qjiang1@inside.artcenter.edu>
2023-10-10 01:03:31 -0700

    1

--- Assets/Script/Cam/RoomManager.cs
+++ Assets/Script/Cam/RoomManager.cs
@@ -11,6 +11,10 @@ public class RoomManager : MonoBehaviour
     public bool room4;
     public bool room4_5;
     public bool room5;
+    public bool room6;
+    public bool room7;
+    public bool room8;
+    public bool room9;
 
     public GameObject room1object;
     public GameObject room2object;
@@ -19,6 +23,10 @@ public class RoomManager : MonoBehaviour
     public GameObject room4object;
     public GameObject room4_5object;
     public GameObject room5object;
+    public GameObject room6object;
+    public GameObject room7object;
+    public GameObject room8object;
+    public GameObject room9object;
 
     private void Start()
     {
@@ -29,6 +37,10 @@ public class RoomManager : MonoBehaviour
         room4object.SetActive(false);
         room4_5object.SetActive(false);
         room5object.SetActive(false);
+        room6object.SetActive(false);
+        room7object.SetActive(false);
+        room8object.SetActive(false);
+        room9object.SetActive(false);
     }
 
     private void Update()
@@ -100,6 +112,42 @@ public class RoomManager : MonoBehaviour
         {
             room5object.SetActive(false);
         }
+
+        if (room6 == true)
+        {
+            room6object.SetActive(true);
+        }
+        else
+        {
+            room6object.SetActive(false);
+        }
+
+        if (room7 == true)
+        {
+            room7object.SetActive(true);
+        }
+        else
+        {
+            room7object.SetActive(false);
+        }
+
+        if (room8 == true)
+        {
+            room8object.SetActive(true);
+        }
+        else
+        {
+            room8object.SetActive(false);
+        }
+
+        if (room9 == true)
+        {
+            room9object.SetActive(true);
+        }
+        else
+        {
+            room9object.SetActive(false);
+        }
     }
 
     public void Cube1()
@@ -136,6 +184,30 @@ public class RoomManager : MonoBehaviour
     {
         room4_5 = !room4_5;
         room5 = !room5;
+    }
+
+    public void Cube7()
+    {
+        room4 = !room4;
+        room6 = !room6;
+    }
+
+    public void Cube8()
+    {
+        room4 = !room4;
+        room7 = !room7;
+    }
+
+    public void Cube9()
+    {
+        room7 = !room7;
+        room8 = !room8;
+    }
+
+    public void Cube10()
+    {
+        room8 = !room8;
+        room9 = !room9;
     }
 
 }
